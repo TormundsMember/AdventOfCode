04426ba1d72d52be8106d5e0327e9d3ba70d9d05
TormundsMember <thisistotallylegit@outlook.com>
2017-06-20 21:13:35 +0200

    added code for AoC day 4

--- src/com/example/programs/Advent4.kt
+++ src/com/example/programs/Advent4.kt
@@ -1,5 +1,9 @@
 package com.example.programs
 
+import com.example.Util
+import com.example.Util.loadFileIntoList
+import javafx.collections.transformation.SortedList
+
 /**
  * Created by Dominik Gudic on 20.06.2017.
  */
@@ -13,11 +17,36 @@ object Advent4 {
     }
 
 
-    fun partOne(){
-        
+    fun partOne() {
+        val codes = loadFileIntoList("advent4.txt")
+
+        var sortedLetters = listOf<Char>().toSortedSet()
+        val amountOf = HashMap<String, Int>().withDefault { 0 }
+
+        for (code in codes) {
+            val lastIndex = code.lastIndexOf('-')
+            val letters = code.subSequence(0, lastIndex).replace(Regex("-"), "")
+            val sectorId = code.subSequence(lastIndex, code.length).replace(Regex("\\[.+]"), "").toInt()
+            val checkSum = code.subSequence(code.indexOf('['), code.length)
+            sortedLetters = letters.toSortedSet()
+            amountOf.clear()
+            for (c in sortedLetters) {
+                if (amountOf.containsKey(c.toString())) {
+                    val i: Int = amountOf[c.toString()]!!
+                    amountOf[c.toString()] = i + 1
+                } else {
+                    amountOf[c.toString()] = 1
+                }
+            }
+            val keys = amountOf.keys
+
+            for (key in keys) {
+
+            }
+        }
     }
 
-    fun partTwo(){
+    fun partTwo() {
 
     }
 }
